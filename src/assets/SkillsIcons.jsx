e49d751ea89ce19dcf7a8326b51f4d9e76b55b94
Imari Childress <imaricodes@gmail.com>
2023-05-19 11:12:03 -0400

    Update skills section, resize skills icons

--- src/assets/SkillsIcons.jsx
+++ src/assets/SkillsIcons.jsx
@@ -1,13 +1,13 @@
 const SkillsIcons = {
     iconGit : new URL('./icon-git.svg', import.meta.url).href,
-    // iconAdobe : new URL('./icon-adobe-creative-cloud.svg', import.meta.url).href,
+    iconAdobe : new URL('./icon-adobe-creative-cloud.svg', import.meta.url).href,
     iconCSS : new URL('./icon-css.svg', import.meta.url).href,
     iconGoogleCloud : new URL('./icon-google-cloud.svg', import.meta.url).href,
-    // iconHTML5 : new URL('./skills-icon-html5.svg', import.meta.url).href,
-    // iconJavascript : new URL('./skills-icon-javascript.svg', import.meta.url).href,
+    iconHTML5 : new URL('./icon-html5.svg', import.meta.url).href,
+    iconJavascript : new URL('./icon-javascript.svg', import.meta.url).href,
     iconReact : new URL('./icon-react.svg', import.meta.url).href,
     iconTailwindCSS : new URL('./icon-tailwindcss.svg', import.meta.url).href,
-    // iconVSCode : new URL('./skills-icon-visual-studio-code.svg', import.meta.url).href,
+    iconVSCode : new URL('./icon-visual-studio-code.svg', import.meta.url).href,
     iconApi : new URL('./icon-api.svg', import.meta.url).href,
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,97 +1,99 @@
 import React from "react";
 import { motion } from "framer-motion";
-import ImagesSkills from "../assets/Images";
+import SkillsIcons from "../assets/SkillsIcons";
 
 const Skills = () => {
   const imageAnimate = {
     offScreen: {
-      y: 100,
+      // y: 100,
       opacity: 0,
     },
     onScreen: {
       y: 0,
       opacity: 1,
       transition: {
-        duration: 0.25,
+        duration: 0.5,
         type: "spring",
       },
     },
   };
 
   return (
-    <motion.div
-      className=" skills"
-      initial={"offScreen"}
-      whileInView={"onScreen"}
-      viewport={{ once: true, amount: 0.2 }}
-      transition={{ staggerChildren: 0.25 }}
-    >
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container  w-[160px]">
-          <img src={ImagesSkills.iconAdobe} alt="adobe-icon" />
-        </div>
-        <span className="skill-containter__title">Adobe Creative Suite</span>
-      </motion.div>
+    <>
+      <motion.div
+        className="skills"
+        initial={"offScreen"}
+        whileInView={"onScreen"}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ staggerChildren: 0.15 }}
+      >
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconReact} alt="react-icon" />
+          </div>
+          <span className="skill-containter__title">React</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container w-[102px]">
-          <img src={ImagesSkills.iconCSS} alt="css-icon" />
-        </div>
-        <span className="skill-containter__title">CSS</span>
-      </motion.div>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconTailwindCSS} alt="tailwind-css-icon" />
+          </div>
+          <span className="skill-containter__title">Tailwind CSS</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div
-          className="skill-container__icon-container w-[118px]"
-          variants={imageAnimate}
-        >
-          <img src={ImagesSkills.iconGit} alt="git-icon" />
-        </div>
-        <span className="skill-containter__title">GIT</span>
-      </motion.div>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconGoogleCloud} alt="google-cloud-icon" />
+          </div>
+          <span className="skill-containter__title">Google Cloud</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container w-[145px]">
-          <img src={ImagesSkills.iconGoogleCloud} alt="google-cloud-icon" />
-        </div>
-        <span className="skill-containter__title">Google Cloud</span>
-      </motion.div>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconApi} alt="rest-api-icon" />
+          </div>
+          <span className="skill-containter__title">Rest API</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container w-[106px]">
-          <img src={ImagesSkills.iconHTML5} alt="html5-icon" />
-        </div>
-        <span className="skill-containter__title">HTML5</span>
-      </motion.div>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div
+            className="skill-container__icon-container"
+            variants={imageAnimate}
+          >
+            <img src={SkillsIcons.iconGit} alt="git-icon" />
+          </div>
+          <span className="skill-containter__title">GIT</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container w-[120px]">
-          <img src={ImagesSkills.iconJavascript} alt="javascript-icon" />
-        </div>
-        <span className="skill-containter__title">Javascript</span>
-      </motion.div>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconJavascript} alt="javascript-icon" />
+          </div>
+          <span className="skill-containter__title">Javascript</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container w-[120px]">
-          <img src={ImagesSkills.iconReact} alt="react-icon" />
-        </div>
-        <span className="skill-containter__title">React</span>
-      </motion.div>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconCSS} alt="css-icon" />
+          </div>
+          <span className="skill-containter__title">CSS</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container w-[195px]">
-          <img src={ImagesSkills.iconTailwindCSS} alt="tailwind-css-icon" />
-        </div>
-        <span className="skill-containter__title">Tailwind CSS</span>
-      </motion.div>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconHTML5} alt="html5-icon" />
+          </div>
+          <span className="skill-containter__title">HTML5</span>
+        </motion.div>
 
-      <motion.div className="skill-container" variants={imageAnimate}>
-        <div className="skill-container__icon-container w-[120px]">
-          <img src={ImagesSkills.iconVSCode} alt="visual-studio-code-icon" />
-        </div>
-        <span className="skill-containter__title">VS Code</span>
+        <motion.div className="skill-container" variants={imageAnimate}>
+          <div className="skill-container__icon-container">
+            <img src={SkillsIcons.iconAdobe} alt="adobe-icon" />
+          </div>
+          <span className="skill-containter__title">Adobe Creative Suite</span>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </>
   );
 };
 
