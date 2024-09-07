5c7cefc3753b7afda62e3d08df54210433781bc1
Imari Childress <imaricodes@gmail.com>
2024-09-07 09:44:00 -0400

    Update description for memorial project, update footer copyright year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   let copyright = String.fromCodePoint(0x00A9);
   return (
     <div className="footer h-14 bg-slate-900 text-white flex items-center justify-center">
-      <p> <span>{copyright} </span>Childress 2003</p>
+      <p> <span>{copyright} </span>Imari Childress 2025</p>
     </div>
   )
 }
